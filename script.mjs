23f988e9bba42d615c40ebb81d42b7cb3018d555
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-06-03 22:43:33 -0700

    New api project now is switched to weather

--- script.mjs
+++ script.mjs
@@ -1,29 +1,30 @@
-async function fetchData() {
-   try {
-    // Two lines. Fetches data. All we need in most cases
-    const result = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const data = await result.json();
+// async function fetchData() {
+//    try {
+//     // Two lines. Fetches data. All we need in most cases
+//     const result = await fetch("https://pokeapi.co/api/v2/pokemon");
+//     const data = await result.json();
   
-    // using pokePokeContainer div from html
-    const pokemonContainer = document.getElementById(`pokePokeContainer`);
-    // Loops through the list of pokemon fetched from the api
-    for (let i = 0; i < data.results.length; i++) {
-      // create a new h1 element
-      const nameDisplay = document.createElement("h1");
+//     // using pokePokeContainer div from html
+//     const pokemonContainer = document.getElementById(`pokePokeContainer`);
+//     // Loops through the list of pokemon fetched from the api
+//     for (let i = 0; i < data.results.length; i++) {
+//       // create a new h1 element
+//       const nameDisplay = document.createElement("h1");
   
-      // sets the text of the element to pokemons name
-      nameDisplay.innerText = data.results[i].name;
+//       // sets the text of the element to pokemons name
+//       nameDisplay.innerText = data.results[i].name;
 
-      // Append to container
-      pokemonContainer.appendChild(nameDisplay);
+//       // Append to container
+//       pokemonContainer.appendChild(nameDisplay);
   
-      // displays the element by appending it to the body
-      document.querySelector("body").appendChild(nameDisplay);
-    }
-    // If out of bound or error were to happen then throw this error message.
-  } catch (error) {
-        console.error(`Error fetching the data`, error);
-  }
-}
-// calling function to get pokemon names
-  fetchData();
+//       // displays the element by appending it to the body
+//       document.querySelector("body").appendChild(nameDisplay);
+//     }
+//     // If out of bound or error were to happen then throw this error message.
+//   } catch (error) {
+//         console.error(`Error fetching the data`, error);
+//   }
+// }
+// // calling function to get pokemon names
+//   fetchData();
+
