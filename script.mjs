6a761a70debeeea00c8a6a0ab853a962372c9efc
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-06-04 22:49:48 -0700

    everything works and is finished. Now have to add missing features required by rubric

--- script.mjs
+++ script.mjs
@@ -1,33 +1,3 @@
-// async function fetchData() {
-//    try {
-//     // Two lines. Fetches data. All we need in most cases
-//     const result = await fetch("https://pokeapi.co/api/v2/pokemon");
-//     const data = await result.json();
-
-//     // using pokePokeContainer div from html
-//     const pokemonContainer = document.getElementById(`pokePokeContainer`);
-//     // Loops through the list of pokemon fetched from the api
-//     for (let i = 0; i < data.results.length; i++) {
-//       // create a new h1 element
-//       const nameDisplay = document.createElement("h1");
-
-//       // sets the text of the element to pokemons name
-//       nameDisplay.innerText = data.results[i].name;
-
-//       // Append to container
-//       pokemonContainer.appendChild(nameDisplay);
-
-//       // displays the element by appending it to the body
-//       document.querySelector("body").appendChild(nameDisplay);
-//     }
-//     // If out of bound or error were to happen then throw this error message.
-//   } catch (error) {
-//         console.error(`Error fetching the data`, error);
-//   }
-// }
-// // calling function to get pokemon names
-//   fetchData();
-
 const weatherForm = document.querySelector(`.weatherForm`);
 const cityInput = document.querySelector(`.cityInput`);
 const card = document.querySelector(`.card`);
@@ -66,40 +36,57 @@ async function getWeatherData(city) {
 function displayWeatherInfo(data) {
     // console.log(data);
     const { name: city,
-            main: { temp, humidity },
-            weather: [{ description, id }]} = data;
+        main: { temp, humidity },
+        weather: [{ description, id }] } = data;
 
-            card.textContent = ``;
-            card.style.display = `flex`;
+    card.textContent = ``;
+    card.style.display = `flex`;
 
-            const cityDisplay = document.createElement(`h1`);
-            const tempDisplay = document.createElement(`p`);
-            const humidityDisplay = document.createElement(`p`);
-            const descriptionDisplay = document.createElement(`p`);
-            const weatherEmoji = document.createElement(`p`);
+    const cityDisplay = document.createElement(`h1`);
+    const tempDisplay = document.createElement(`p`);
+    const humidityDisplay = document.createElement(`p`);
+    const descriptionDisplay = document.createElement(`p`);
+    const weatherEmoji = document.createElement(`p`);
 
-            cityDisplay.textContent = city;
-            tempDisplay.textContent = `${((temp - 273.15) * 9/5 + 32).toFixed(1)}°F`;
-            humidityDisplay.textContent = `Humidity: ${humidity}%`;
-            descriptionDisplay.textContent = description;
-            weatherEmoji.textContent = getWeatherEmoji(id);
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${((temp - 273.15) * 9 / 5 + 32).toFixed(1)}°F`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descriptionDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
 
-            cityDisplay.classList.add(`cityDisplay`);
-            tempDisplay.classList.add(`tempDisplay`);
-            humidityDisplay.classList.add(`humidityDisplay`);
-            descriptionDisplay.classList.add(`descriptionDisplay`);
-            weatherEmoji.classList.add(`weatherEmoji`);
+    cityDisplay.classList.add(`cityDisplay`);
+    tempDisplay.classList.add(`tempDisplay`);
+    humidityDisplay.classList.add(`humidityDisplay`);
+    descriptionDisplay.classList.add(`descriptionDisplay`);
+    weatherEmoji.classList.add(`weatherEmoji`);
 
-            card.appendChild(cityDisplay);
-            card.appendChild(tempDisplay);
-            card.appendChild(humidityDisplay);
-            card.appendChild(descriptionDisplay);
-            card.appendChild(weatherEmoji);
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descriptionDisplay);
+    card.appendChild(weatherEmoji);
 
 }
 
 function getWeatherEmoji(weatherId) {
-    
+    switch (true) {
+        case (weatherId >= 200 && weatherId < 300):
+            return `🌩`;
+        case (weatherId >= 300 && weatherId < 400):
+            return `🌧`;
+        case (weatherId >= 500 && weatherId < 600):
+            return `🌧`;
+        case (weatherId >= 600 && weatherId < 700):
+            return `🌨`;
+        case (weatherId >= 700 && weatherId < 800):
+            return `🌫`;
+        case (weatherId === 800):
+            return `🌞`;
+        case (weatherId >= 801 && weatherId < 810):
+            return `☁️`;
+        default:
+            return `❓`;
+    }
 }
 
 function displayError(message) {
@@ -110,3 +97,33 @@ function displayError(message) {
     card.style.display = `flex`;
     card.appendChild(errorDisplay);
 }
+
+// async function fetchData() {
+//    try {
+//     // Two lines. Fetches data. All we need in most cases
+//     const result = await fetch("https://pokeapi.co/api/v2/pokemon");
+//     const data = await result.json();
+
+//     // using pokePokeContainer div from html
+//     const pokemonContainer = document.getElementById(`pokePokeContainer`);
+//     // Loops through the list of pokemon fetched from the api
+//     for (let i = 0; i < data.results.length; i++) {
+//       // create a new h1 element
+//       const nameDisplay = document.createElement("h1");
+
+//       // sets the text of the element to pokemons name
+//       nameDisplay.innerText = data.results[i].name;
+
+//       // Append to container
+//       pokemonContainer.appendChild(nameDisplay);
+
+//       // displays the element by appending it to the body
+//       document.querySelector("body").appendChild(nameDisplay);
+//     }
+//     // If out of bound or error were to happen then throw this error message.
+//   } catch (error) {
+//         console.error(`Error fetching the data`, error);
+//   }
+// }
+// // calling function to get pokemon names
+//   fetchData();
